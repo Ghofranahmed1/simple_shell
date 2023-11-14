6e50f46efe3914f15d5258a64e7ba578e46aced9
Ghofranahmed1 <gofranahmed40@gmail.com>
2023-11-13 16:18:02 -0800

    all

--- main.c
+++ main.c
@@ -15,18 +15,19 @@ int main (int argc, char *argv[])
 		get_line = get_command();
 		if (get_line != NULL)
 		{
-		tokens = tokenizing(get_line);
-		if (tokens == NULL)
-		{
-			free(get_line);
-			continue;
-		}
-		if (!strcmp(tokens[0], "exit"))
+			tokens = tokenizing(get_line);
+			if (tokens == NULL)
+			{
+				free(get_line);
+				continue;
+			}
+			if (!strcmp(tokens[0], "exit"))
 				exit(tokens, get_line, exit_satus);
-		if (!strcmp(token[0], "env"))
-				getenv();
-		else 
-		{
+			if (!strcmp(token[0], "env"))
+				getenv(env);
+			else
+			{
+
 
 
 
