deacec1d6968413649c6c34ba98e56986cb071d6
Ghofranahmed1 <gofranahmed40@gmail.com>
2023-11-15 14:35:25 -0800

    some editing

--- main.h
+++ main.h
@@ -7,12 +7,12 @@
 #include <stdlib.h>
 #include <string.h>
 #include <stddef.h>
-#include <stdlib.h>
 #include <sys/types.h>
 #include <sys/wait.h>
-#include <sys/types.h>
 #include <sys/stat.h>
 #include <errno.h>
+
+extern char **environ;
 
 void exit_command(char **token_array, char *get_line, int status);
 int command_execution(char **array_token, char **argv, char **env, char *lineptr, int path_value, int path_fun_return);
