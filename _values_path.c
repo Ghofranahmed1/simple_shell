53315ef8baa52bb37d6a55daf0ad124caf7d6eee
Ghofranahmed1 <gofranahmed40@gmail.com>
2023-11-16 07:56:14 -0800

    use strtok

--- _values_path.c
+++ _values_path.c
@@ -16,20 +16,20 @@ int _values_path(char **arg, char **env)
 	path_rela = _get_path(env);
 	if (!path_rela)
 		return (-1);
-	token = _strtok(path_rela, ":");
-	command = _strlen(*arg);
+	token = strtok(path_rela, ":");
+	command = strlen(*arg);
 	while (token)
 	{
-		value_path = _strlen(token);
+		value_path = strlen(token);
 		path_absol = malloc(sizeof(char) * (value_path + command + 2));
 		if (!path_absol)
 		{
 			free(path_rela);
 			return (-1);
 		}
-		path_absol = _strcpy(path_absol, token);
-		_strcat(path_absol, "/");
-		_strcat(path_absol, *arg);
+		path_absol = strcpy(path_absol, token);
+		strcat(path_absol, "/");
+		strcat(path_absol, *arg);
 
 		if (stat(path_absol, &stat_lineptr) == 0)
 		{
@@ -38,7 +38,7 @@ int _values_path(char **arg, char **env)
 			return (0);
 		}
 		free(path_absol);
-		token = _strtok(NULL, ":");
+		token = strtok(NULL, ":");
 	}
 	free(path_rela);
 	return (-1);
