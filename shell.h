aba94bdb956878409387e22536bce200545cdada
Ghofranahmed1 <gofranahmed40@gmail.com>
2023-11-16 07:57:44 -0800

    use strtok

--- shell.h
+++ shell.h
@@ -28,7 +28,6 @@ char **_get_token(char *lineptr);
 void _exit_command(char **args, char *lineptr, int _exit);
 int _fork_fun(char **arg, char **av, char **env,
 char *lineptr, int np, int c);
-char *_strtok(char *str, const char *delim);
 
 
 #endif /* SHELL_H */
